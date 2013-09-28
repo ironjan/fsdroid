30977c60c49773036b41050fa78923cb84bd4e08
Jan Lippert <ljan@mail.upb.de>
2013-09-28 18:58:38 +0200

    Added basic news implementation

--- fsdroid/fsdroid/src/main/java/de/upb/fsmi/cards/views/DummyNewsCardView.java
+++ fsdroid/fsdroid/src/main/java/de/upb/fsmi/cards/views/DummyNewsCardView.java
@@ -1,28 +1,65 @@
 package de.upb.fsmi.cards.views;
 
-import android.content.*;
-import android.net.*;
-import android.widget.*;
+import java.text.SimpleDateFormat;
 
-import com.googlecode.androidannotations.annotations.*;
-import com.googlecode.androidannotations.annotations.res.*;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-import de.upb.fsmi.*;
+import com.google.code.rome.android.repackaged.com.sun.syndication.feed.rss.Description;
+import com.google.code.rome.android.repackaged.com.sun.syndication.feed.rss.Item;
+import com.googlecode.androidannotations.annotations.Click;
+import com.googlecode.androidannotations.annotations.EViewGroup;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringRes;
+
+import de.upb.fsmi.R;
 
 @EViewGroup(R.layout.card_news_dummy)
 public class DummyNewsCardView extends RelativeLayout {
 	@StringRes
 	String die_fachschaft_de;
 
-	@Click({ R.id.imgNewsLink, R.id.dummyNewsExplanation })
+	@ViewById
+	TextView newsHeadline, newsDate;
+	@ViewById
+	WebView newsText;
+	@ViewById
+	ImageView imgNewsLink;
+
+	private String mLink;
+
+	@Click({ R.id.imgNewsLink })
 	void goToWebsite() {
-		Uri fsmiWebsite = Uri.parse(die_fachschaft_de);
+		if ("".equals(mLink)) {
+			imgNewsLink.setVisibility(View.GONE);
+			return;
+		}
+		Uri fsmiWebsite = Uri.parse(mLink);
 		Intent intent = new Intent(Intent.ACTION_VIEW, fsmiWebsite);
 		getContext().startActivity(intent);
 	}
 
-	public DummyNewsCardView(Context pContext) {
+	public DummyNewsCardView(String title, Context pContext) {
 		super(pContext);
+	}
+
+	public void bind(Item pRssItem) {
+		this.mLink = pRssItem.getLink();
+		newsHeadline.setText(pRssItem.getTitle());
+		newsDate.setText(SimpleDateFormat.getDateTimeInstance().format(
+				pRssItem.getPubDate()));
+		displayItemContent(pRssItem.getDescription());
+	}
+
+	private void displayItemContent(Description pDescription) {
+		//bug in WebView.loadData(data, mimeType, encoding) which fucks up utf-8
+		newsText.loadDataWithBaseURL(null, pDescription.getValue(), "text/html", "utf-8",null);
 	}
 
 }
